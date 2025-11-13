a558e41e27b778cdd46d2c7ebc174c8ae9d55f09
William Conner <redwils5@gmail.com>
2025-11-12 21:26:54 -0500

    Using sessions to handle current login

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
-from flask import Flask, redirect, url_for, render_template, request
+from flask import Flask, redirect, url_for, render_template, request, session
+from datetime import timedelta
 
 app = Flask(__name__)
+app.secret_key = "reallyfriggincoolman"
+app.permanent_session_lifetime = timedelta(days= 1)
 
 @app.route("/")
 def root():
@@ -9,15 +12,24 @@ def root():
 @app.route("/login", methods=["POST", "GET"])
 def login():
     if request.method == "POST":
-        username = request.form["username"]
-        return redirect(url_for("tdlist", usr= username))
+        session.permanent = True
+        user = request.form["username"]
+        session["user"] = user;
+        return redirect(url_for("tdlist"))
     else:
         return render_template("login.html")
 
-@app.route("/list/<usr>")
-def tdlist(usr):
-    return render_template("list.html", name= usr)
+@app.route("/list")
+def tdlist():
+    if "user" in session:
+        user = session["user"]
+        return render_template("list.html", name= user)
+    return redirect(url_for("login"))
 
+@app.route("/logout")
+def logout():
+    session.pop("user", None)
+    return redirect(url_for("login"))
 
 if __name__ == "__main__":
     app.run(debug= True)
